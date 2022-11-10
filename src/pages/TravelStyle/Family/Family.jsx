1d0c976f37e5b3b6f30b120ebc4a830178b08542
TrongVi <vutrongvi96@gmail.com>
2022-11-10 10:49:45 +0700

    10:48 10/11/2022

--- src/pages/TravelStyle/Family/Family.jsx
+++ src/pages/TravelStyle/Family/Family.jsx
@@ -1,18 +1,239 @@
-import React from 'react'
-import './Family.css'
+import React from "react";
+import "./Family.css";
+import { RiMoneyDollarCircleLine, Ri24HoursFill } from "react-icons/ri";
 
 const Family = () => {
   return (
-    <div className='body-family'>
-      <div className='family-content'>
-          Family
+    <div className="body-family">
+      <div className="family-content">Family</div>
+      <div className="detail-family">
+        {" "}
+        A house is built by hands, but a home is built by heart.{" "}
       </div>
-      <div className="detail-family"> A house is built by hands, but a home is built by heart. </div>
-      <div className='family-container'>
-           a 
+      <div className="family-container">
+        <div className="page-up">
+          <div className="title-top"> See The World Together</div>
+          <div className="middle-page"> 
+          We don’t believe “let’s have kids” should mean you have to stop exploring the world.
+           We also don’t believe you should get home from a holiday just as tired as when you left.
+            So we have real parents at B&R design and test-drive everything.
+             Our resulting journeys get rave reviews from young and young-at-heart people alike.
+              Nothing’s dumbed-down, everything’s 100% stress-free, 
+              and there’s always as much action and excitement as the kids can handle.
+          </div>
+          <div className="end-page"> Look By Eyes- Feel By Heart </div>
+        </div>
+        <div className="simpletour-1">
+          <div className="top-container">
+            <div className="box-1"></div>
+            <div className="boxsub-1">
+              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
+              <div className="title-highlight">
+                <ul className="highlight">
+                  <li>
+                    {" "}
+                    Taste Hanoi beer and experience the Vietnamese beer drinking
+                    cultur{" "}
+                  </li>
+                  <li>
+                    Take in a cooking demonstration with the chef to learn about
+                    Vietnamese food, while floating through Halong Bay{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Explore the Imperial Citadel, a UNESCO-designated World
+                    Heritage Site, and sample Hue cuisine for its authentic and
+                    recognizable flavor{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Try the food at the busiest street booths and secret
+                    passageways in Hoi An Town in true native fashion while
+                    riding a motorcycle.{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Experience the vast Mekong Delta's rural villages, farming
+                    practices, and fruit orchards by boat through its winding
+                    canals while sampling local delicacies.
+                  </li>
+                </ul>
+              </div>
+              <div className="read-more">
+                <div className="icon-show">
+                  <div className="money">
+                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
+                  </div>
+                  <div className="day-lenght">
+                    <Ri24HoursFill /> 12 Day
+                  </div>
+                </div>
+                <div className="button-show">
+                  <a href="/"> Show more..</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="simpletour-1">
+          <div className="top-container">
+            <div className="boxsub-1">
+              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
+              <div className="title-highlight">
+                <ul className="highlight">
+                  <li>
+                    {" "}
+                    Taste Hanoi beer and experience the Vietnamese beer drinking
+                    cultur{" "}
+                  </li>
+                  <li>
+                    Take in a cooking demonstration with the chef to learn about
+                    Vietnamese food, while floating through Halong Bay{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Explore the Imperial Citadel, a UNESCO-designated World
+                    Heritage Site, and sample Hue cuisine for its authentic and
+                    recognizable flavor{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Try the food at the busiest street booths and secret
+                    passageways in Hoi An Town in true native fashion while
+                    riding a motorcycle.{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Experience the vast Mekong Delta's rural villages, farming
+                    practices, and fruit orchards by boat through its winding
+                    canals while sampling local delicacies.
+                  </li>
+                </ul>
+              </div>
+              <div className="read-more">
+                <div className="icon-show">
+                  <div className="money">
+                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
+                  </div>
+                  <div className="day-lenght">
+                    <Ri24HoursFill /> 12 Day
+                  </div>
+                </div>
+                <div className="button-show">
+                  <a href="/"> Show more..</a>
+                </div>
+              </div>
+            </div>
+            <div className="box-2"></div>
+          </div>
+        </div>
+        <div className="simpletour-1">
+          <div className="top-container">
+            <div className="box-3"></div>
+            <div className="boxsub-1">
+              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
+              <div className="title-highlight">
+                <ul className="highlight">
+                  <li>
+                    {" "}
+                    Taste Hanoi beer and experience the Vietnamese beer drinking
+                    cultur{" "}
+                  </li>
+                  <li>
+                    Take in a cooking demonstration with the chef to learn about
+                    Vietnamese food, while floating through Halong Bay{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Explore the Imperial Citadel, a UNESCO-designated World
+                    Heritage Site, and sample Hue cuisine for its authentic and
+                    recognizable flavor{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Try the food at the busiest street booths and secret
+                    passageways in Hoi An Town in true native fashion while
+                    riding a motorcycle.{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Experience the vast Mekong Delta's rural villages, farming
+                    practices, and fruit orchards by boat through its winding
+                    canals while sampling local delicacies.
+                  </li>
+                </ul>
+              </div>
+              <div className="read-more">
+                <div className="icon-show">
+                  <div className="money">
+                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
+                  </div>
+                  <div className="day-lenght">
+                    <Ri24HoursFill /> 12 Day
+                  </div>
+                </div>
+                <div className="button-show">
+                  <a href="/"> Show more..</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="simpletour-1">
+          <div className="top-container">
+            <div className="boxsub-1">
+              <div className="title-box">HA LONG 4 DAY - 3 NIGHT</div>
+              <div className="title-highlight">
+                <ul className="highlight">
+                  <li>
+                    {" "}
+                    Taste Hanoi beer and experience the Vietnamese beer drinking
+                    cultur{" "}
+                  </li>
+                  <li>
+                    Take in a cooking demonstration with the chef to learn about
+                    Vietnamese food, while floating through Halong Bay{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Explore the Imperial Citadel, a UNESCO-designated World
+                    Heritage Site, and sample Hue cuisine for its authentic and
+                    recognizable flavor{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Try the food at the busiest street booths and secret
+                    passageways in Hoi An Town in true native fashion while
+                    riding a motorcycle.{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    Experience the vast Mekong Delta's rural villages, farming
+                    practices, and fruit orchards by boat through its winding
+                    canals while sampling local delicacies.
+                  </li>
+                </ul>
+              </div>
+              <div className="read-more">
+                <div className="icon-show">
+                  <div className="money">
+                    <RiMoneyDollarCircleLine /> FROM 1054${" "}
+                  </div>
+                  <div className="day-lenght">
+                    <Ri24HoursFill /> 12 Day
+                  </div>
+                </div>
+                <div className="button-show">
+                  <a href="/"> Show more..</a>
+                </div>
+              </div>
+            </div>
+            <div className="box-4"></div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Family
+export default Family;
